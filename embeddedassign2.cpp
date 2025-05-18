7d693d6b9ed2aa43d182450ddedf065723f6f314
skylune <3223983637@qq.com>
2025-05-18 21:45:22 +0800

    Update embeddedassign2.cpp

--- embeddedassign2.cpp
+++ embeddedassign2.cpp
@@ -1,39 +1,77 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+//定义使用的串口
+const int LEDPIN1 = 25;
+const int LEDPIN2 = 26;
 
-#define WIDTH   128   // OLED display width, in pixels
-#define HEIGHT  64    // OLED display height, in pixels
-// 软件 SPI 总线
-// Declaration for SSD1306 display connected using software SPI (default case):
-#define OLED_SCK  18
-#define OLED_SDA  13
-#define OLED_RES  15
-#define OLED_DC   2
-#define OLED_CS   4
+//记录LED当前状态，-1为灭，1为亮
+int flag1=-1;
+int flag2=-1;
 
-// 构造对象
-Adafruit_SSD1306  OLED(WIDTH, HEIGHT, OLED_SDA, OLED_SCK, OLED_DC, OLED_RES, OLED_CS);
+void processcommand(char command);
+
 
 void setup() {
-  // OLED初始化
-  OLED.begin();
-  // OLED清除显示
-  OLED.clearDisplay();
-  // 设置光标位置
-  OLED.setCursor(1, 1);
-  // 设置文本颜色
-  OLED.setTextColor(SSD1306_WHITE);
-  // 设置字体大小
-  OLED.setTextSize(2);
-  // 显示字符
-  OLED.println("HelloWorld");
+    // 设定引脚为输出模式
+    pinMode(LEDPIN1,OUTPUT);
+    pinMode(LEDPIN2,OUTPUT);
 
-  // 显示内容
-  OLED.display();
+    //初始化为熄灭
+    digitalWrite(LEDPIN1,LOW);
+    digitalWrite(LEDPIN2,LOW);
+    //初始化串口通信
+    Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    //检查串口
+    if(Serial.available()>0){
+        char command = Serial.read();
+
+        //等待数据传输完成并清空
+        delay(50);
+        while(Serial.available()>0){
+            Serial.read();
+        }
+
+        //开始处理
+        processcommand(command);
+    }
+}
+
+void processcommand(char command){
+    switch(command){
+        case '1':
+            //25号
+            if(flag1==-1){
+                flag1=-1*flag1;
+                digitalWrite(LEDPIN1,HIGH);
+                Serial.println("LED1亮");
+                break;
+            }else{
+                flag1=-1*flag1;
+                digitalWrite(LEDPIN1,LOW);
+                Serial.println("LED1灭");
+                break;
+            }
+
+            case '2':
+            //26号
+            if(flag2==-1){
+                flag2=-1*flag2;
+                digitalWrite(LEDPIN2,HIGH);
+                Serial.println("LED2亮");
+                break;
+            }else{
+                flag2=-1*flag2;
+                digitalWrite(LEDPIN2,LOW);
+                Serial.println("LED2灭");
+                break;
+            }    
+            default:
+            //未知指令
+            Serial.print("未知指令：");
+            Serial.println(command);
+            break;
+    }
 }
